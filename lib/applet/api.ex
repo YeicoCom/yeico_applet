648049b02c11bd055531c81fea602f564ba114b5
Samuel Ventura <samuel.ventura@yeico.com>
2025-06-01 02:03:48 -0600

    Bug fix. Api await default to infinity.

--- lib/applet/api.ex
+++ lib/applet/api.ex
@@ -22,7 +22,7 @@ defmodule Applet.Api do
   def sleep(), do: :timer.sleep(:infinity)
   def sleep(millis), do: :timer.sleep(millis)
   def pid(%Task{pid: pid}), do: pid
-  def await(task, timeout \\ 5_000), do: call({:await, task, timeout})
+  def await(task, timeout \\ :infinity), do: call({:await, task, timeout})
 
   def async(fun) when is_function(fun, 0), do: call({:async, fun})
 
