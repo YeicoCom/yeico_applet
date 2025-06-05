2b563c2d1a1d1b8f4731451567188b5e9b9c3d42
Samuel Ventura <samuel.ventura@yeico.com>
2025-06-04 23:31:59 -0600

    Added socket errors test.

---
+++ test/applet_tcp_udp_test.exs
@@ -0,0 +1,23 @@
+defmodule AppletTcpUdpTest do
+  use ExUnit.Case, async: false
+  use Applet.Alias
+  use Applet.Api
+
+  setup do
+    Utils.wait_success(20, 20, fn -> assert [] = Multiple.list() end)
+  end
+
+  test "tcp listen and connect error" do
+    {:ok, socket} = Tcp.listen("127.0.0.1", 0)
+    {:error, _} = Tcp.listen("127.0.0.1", socket.port)
+    :ok = Tcp.close(socket)
+    {:error, _} = Tcp.connect("127.0.0.1", socket.port)
+  end
+
+  test "udp connect error" do
+    {:ok, socket} = Udp.listen("127.0.0.1", 0)
+    {:error, _} = Udp.listen("127.0.0.1", socket.port)
+    {:ok, socket} = Udp.connect("127.0.0.1", 8000)
+    {:error, _} = Udp.listen("127.0.0.1", socket.port)
+  end
+end
